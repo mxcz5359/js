437bd0b63f5f0c9e0f2a045f313f946be2b319bf
mxcz5359 <156735121+mxcz5359@users.noreply.github.com>
2025-07-05 21:57:44 +0800

    Update 2.js

--- 22.js
+++ 22.js
@@ -1,10 +1,10 @@
 window.CustomBackgroundImage="https://php.535359.xyz/"; /* 页面背景图 */
-window.CustomLogo = "https://raw.githubusercontent.com/mxcz5359/surge/refs/heads/main/favicon.ico"; /* 自定义Logo */
+window.CustomLogo = "https://img.535359.xyz/i/2025/07/05/9984209a3bdfe6f8001.jpg"; /* 自定义Logo */
 window.ShowNetTransfer  = "true"; /* 卡片显示上下行流量 */
 /*window.DisableAnimatedMan  = "true";*/     /* 关掉动画人物插图 */
 window.CustomIllustration = 'https://img.535359.xyz/i/2025/07/05/126aa6d917803f8d001.webp';
 window.CustomDesc ="MyVps"; /* 自定义描述 */
-window.CustomLinks = '[{"link":"https://mybox.mxcz.xyz:5359/","name":"Mybox"},{"link":"https://ai.mxcz.xyz:5359/","name":"Ai"}]';
+window.CustomLinks = '[{"link":"https://mybox.535359.xyz/","name":"Mybox"},{"link":"https://ai.535359.xyz:5359/","name":"Ai"}]';
 
 // 自定义字体：LXGW WenKai
 var link = document.createElement('link');
