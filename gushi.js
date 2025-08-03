bffaedb1a51553074b3b625b771fb982be251032
mxcz5359 <156735121+mxcz5359@users.noreply.github.com>
2025-08-03 14:23:33 +0800

    Update gushi.js

--- gushi.js
+++ gushi.js
@@ -1,8 +1,8 @@
-  setInterval(() => {
+setInterval(() => {
     fetch('https://v1.jinrishici.com/all.txt')
-      。then(response => response.text())
-      。then(data => {
+      .then(response => response.text())
+      .then(data => {
         document.title = data; // 每 5 秒更新标题
       })
-      。catch(error => console.error('Error fetching poem:', error));
+      .catch(error => console.error('Error fetching poem:', error));
   }, 5000);
